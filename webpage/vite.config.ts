3297805e7f2e2d2e8cd1dcd6330b4082ff8ed1bf
Kevin Tan <ky2tan@uwaterloo.ca>
2025-01-08 01:02:00 -0500

    pls 2

--- webpage/vite.config.ts
+++ webpage/vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/Limbus-ID-Graph/',
   assetsInclude: ['**/*.yaml', '**/*.yml'], // Tell Vite to handle YAML files as assets
 })
